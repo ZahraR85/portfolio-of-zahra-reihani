5cd317df0e00d6ab39547972bb11935d49ca576c
Shabnam <zahra.reihani85@gmail.com>
2025-08-27 17:55:29 +0200

    Add basenam in BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+import React from "react"; // Add this at the top
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
+import App from "./App.jsx";
 import "./index.css";
 import "./components/style/Style.css";
-
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
